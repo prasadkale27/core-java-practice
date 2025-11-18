1fa22163f4594c56c76b73b26f7f226c701643ad
prasadkale27 <prasadkale2727@gmail.com>
2025-11-19 00:16:10 +0530

    java.lang.NumberFormatException string cannot convert it into double

--- practice/day1/src/Sum.java
+++ practice/day1/src/Sum.java
@@ -1,9 +1,10 @@
 class Sum {
     public static void main (String[]args){
 	double num1=Double.parseDouble(args[0]);
+
 	double num2=Double.parseDouble(args[1]);
 	System.out.println("Sum " +(num1 + num2));
 	
-        
+         
     }
 }
