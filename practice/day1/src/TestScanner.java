b188c28ba535e8b2da03aabaa69bf8a12e93bf57
prasadkale27 <prasadkale2727@gmail.com>
2025-11-19 14:43:18 +0530

    scanner class system in out err printing and 1 st day end

---
+++ practice/day1/src/TestScanner.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class TestScanner{
+
+public static void main(String []args)
+{
+
+Scanner sc=new Scanner(System.in);
+
+System.out.println("Enter Two Number For Multipplication");
+double num1=sc.nextDouble();
+double num2=sc.nextDouble();
+
+System.out.printf("Result= %.3f %n",(num1*num2));
+
+
+
+
+
+
+}
+
+
+
+}
