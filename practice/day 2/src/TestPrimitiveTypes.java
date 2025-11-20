8a79346f2915c5c654e75e43fb51bfbdec5e60eb
prasadkale27 <prasadkale2727@gmail.com>
2025-11-20 21:41:10 +0530

    error: incompatible types: possible lossy conversion from int to byte

---
+++ practice/day 2/src/TestPrimitiveTypes.java
@@ -0,0 +1,9 @@
+public class TestPrimitiveTypes {
+
+
+public static void main(String []  args )
+{
+
+		byte b1=128;// error: incompatible types: possible lossy conversion from int to byte
+} }
+//compile time error
