b2981361cc50a489fda59fa03a68470c44eede2f
prasadkale27 <prasadkale2727@gmail.com>
2025-11-20 20:07:36 +0530

    error: incompatible types: int cannot be converted to boolean

---
+++ practice/day 2/src/Test2.java
@@ -0,0 +1,13 @@
+public class Test2 {
+
+		public static void main(String []args )
+{
+	
+	int count=100;
+	if(count)//  javac err
+
+	System.out.println("yess");
+	else
+	System.out.println("No");
+}
+}
