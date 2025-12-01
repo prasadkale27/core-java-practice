1b46d109cabce283ae720c5d207f356ce2d04e8a
prasadkale27 <prasadkale2727@gmail.com>
2025-12-01 12:33:54 +0530

    Resource leak: 'sc' is never closed

--- practice/day 2/src/TestPrimitiveTypes.java
+++ practice/day 2/src/TestPrimitiveTypes.java
@@ -4,6 +4,14 @@ public class TestPrimitiveTypes {
 public static void main(String []  args )
 {
 
-		byte b1=128;// error: incompatible types: possible lossy conversion from int to byte
+		byte b1=10;// error: incompatible types: possible lossy conversion from int to byte
+		byte b2=20;
+		byte b3=b1+b2;//RHS : int , LHS  : byte --javac err
+		int b3=b1+b2;//no javac error
+		byte b4=(byte)(b1+b2);//no javac err typecasting or widening 	 loose some of the precision 
+
+ 
+
+
 } }
 //compile time error
---
+++ practice/day4/src/CreateBox.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class CreateBox	
+{
+
+public static void main(String [] args )
+{
+
+	//create scanner's instance to wrap std i/p
+	
+	Scanner sc=new Scanner(System.in);
+	
+	//close scanner 
+	sc.close();
+	
+
+}
+
+
+
+
+}
