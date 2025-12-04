7fa4832291b58b8c5e8360b0adae051b708dac2c
prasadkale27 <prasadkale2727@gmail.com>
2025-12-04 19:45:36 +0530

    box dimension code and leak resources due to sc.closed

--- practice/day 3/src/Box.java
+++ practice/day 3/src/Box.java
@@ -1,28 +1,32 @@
-// default access specifier available within the same packages 
-class Box 
-{
-// state of the Box : tight encapsulation 
+	// default access specifier available within the same packages 
+	class Box 
+	{
+	
+	// state of the Box : tight encapsulation 
 
-private double width , depth , height ;// instance variables
-//parameterized constructor to init complete state of the box 
+	private double width , depth , height ;// instance variables
+	
+	//parameterized constructor to init complete state of the box 
 
-Box(double w, double d, double h)
-{
+	Box(double w, double d, double h)
+	{
 
-width=w;
-depth=d;
-this.height=height;
+	width=w;
+	depth=d;
+	this.height=height;
 
-//this keyword is used to un-hide instance var from local var 
-//height=height;// no error but it give you wrong result means method local variable its shadowing 
-}
-//behaviour : methods  
-//write a non static (instance method) to return Box details in String form (dimensions of Box)
+	//this keyword is used to un-hide instance var from local var 
+	//height=height;// no error but it give you wrong result means method local variable its shadowing 
+	}
+
+	//behaviour : methods  
+	//write a non static (instance method) to return Box details in String form (dimensions of Box)
 
 	String getBoxDimension(){
 	return "Box Dims "+this.width+ ""+depth+""+height;
 	}
-//write a non-static method To return computed volume of the box. 
+	
+	//write a non-static method To return computed volume of the box. 
 	double getBoxVolume()
 	{
 	
--- practice/day4/src/CreateBox.java
+++ practice/day4/src/CreateBox.java
@@ -1,22 +1,67 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 
-class CreateBox	
-{
+//class CreateBox	
+//{
 
-public static void main(String [] args )
-{
+//public static void main(String [] args )
+//{
 
 	//create scanner's instance to wrap std i/p
 	
-	Scanner sc=new Scanner(System.in);
+//	Scanner sc=new Scanner(System.in);
+//	System.out.println("Enter Box Dimensions: w d h");
+//	Box box1;//ref type of var (class type of ref ) , no object created so far, method local var :
+//	//mem allocated of the stack :as per jvm specification 32 bit - 4 bytes and 64 bit jvm amd 64 bit - 
+//	//System.out.println(box1);javac error 
+//	box1=new Box(sc.nextDouble(),sc.nextDouble(),sc.nextDouble());//box class loaded in method area its instance is created in heap 
+//	//print box dimension 
+//	System.out.println(box1.getBoxDimension());
+	//print box volume 
+	//System.out.println("volume" + Box.getBoxVolume());// compile time error becuase its not a static method 
+//	System.out.println("volume :" + box1.getBoxVolume());// ref type of variable it works at all 
 	
+
+
+
+	//Resource leak: sc is never closed
 	//close scanner 
-	sc.close();
+//	sc.close();
 	
 
-}
+//}
 
 
 
 
+//}
+
+
+
+
+
+
+import java.util.Scanner;
+
+class CreateBox
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Box Dimensions: w d h");
+
+        Box box1 = new Box(
+            sc.nextDouble(),
+            sc.nextDouble(),
+            sc.nextDouble()
+        );
+
+        // Print box dimensions
+        System.out.println(box1.getBoxDimension());
+
+        // Print box volume
+        System.out.println("volume : " + box1.getBoxVolume());
+
+        sc.close();
+    }
 }
