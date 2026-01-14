c00674290f09425fe7a4471fe3c0d4a61183ff95
prasadkale27 <prasadkale2727@gmail.com>
2026-01-15 05:08:20 +0530

    using inherited form of equals :based upon ref equality 2)class cast exception

--- practice/day17/src/com/app/vehicles/Tester.java
+++ practice/day17/src/com/app/vehicles/Tester.java
@@ -10,10 +10,8 @@ public class Tester {
 		System.out.println("Enter 2nd  vehicle details  : regNo,  color,  price");
 		Vehicle vehicle2 = new Vehicle(sc.nextInt(), sc.next(), sc.nextDouble());
 		// test equality of two vehicles
-		System.out.println(vehicle1.equals(vehicle2));// using inherited form of equals :based upon
-		// method to internal representation of address 
-		System.out.println(vehicle1.hashCode() +"   "+  vehicle2.hashCode());
-		System.out.println(vehicle1==vehicle2);
+		System.out.println(vehicle1.equals(vehicle2));// using inherited form of equals :based upon ref equality
+		System.out.println(vehicle1.equals(sc));
 		sc.close();
 	}
 }
--- practice/day17/src/com/app/vehicles/Vehicle.java
+++ practice/day17/src/com/app/vehicles/Vehicle.java
@@ -1,21 +1,30 @@
 package com.app.vehicles;
 
 public class Vehicle {
-		private int regNo;
-		private String color;
-		private double price;
-		
-		public Vehicle(int regNo, String color, double price) {
-		
-			this.regNo = regNo;
-			this.color = color;
-			this.price = price;
-		}
+	private int regNo;
+	private String color;
+	private double price;
 
-		@Override
-		public String toString() {
-			return "Vehicle [regNo=" + regNo + ", color=" + color + ", price=" + price + "]";
-		}
+	public Vehicle(int regNo, String color, double price) {
+
+		this.regNo = regNo;
+		this.color = color;
+		this.price = price;
+	}
+
+//override toString to replace hashcode version by actual state details 
+	@Override
+	public String toString() {
+		return "Vehicle [regNo=" + regNo + ", color=" + color + ", price=" + price + "]";
+	}
+
+//override to equals to replace ref equality to contents equality 
+	 @Override
+	public boolean equals(Object o) {
+		 System.out.println("in vehicle equals");
+		 if(o instanceof Vehicle )
+		return this.regNo==((Vehicle)o).regNo   ;
+		 return false;
 		
-	
+	}											
 }
