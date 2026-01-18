2da45505394c5189e6280d258effd627a9c47440
prasadkale27 <prasadkale2727@gmail.com>
2026-01-18 20:57:18 +0530

    Even though reference type is Object, JVM calls Student's overridden equals() method at runtime because method dispatch is based on object type

---
+++ practice/day19video/src/com/app/students/Student.java
@@ -0,0 +1,45 @@
+package com.app.students;
+
+public class Student {
+	private int rollNo;
+	private String subject;
+	private String firstName;
+	private String lastName;
+	private double gpa;
+
+	public Student(int rollNo, String subject, String firstName, String lastName, double gpa) {
+		super();
+		this.rollNo = rollNo;
+		this.subject = subject;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.gpa = gpa;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [rollNo=" + rollNo + ", subject=" + subject + ", firstName=" + firstName + ", lastName="
+				+ lastName + ", gpa=" + gpa + "]";
+	}
+
+	// replace ref equality by content (rollNo n subject ) equality
+	@Override
+	public boolean equals(Object o) {
+
+		System.out.println("in student equals ");
+		if (o instanceof Student) {
+
+			Student s = (Student) o;
+			return this.rollNo == s.rollNo && this.subject.equals(s.subject);
+			// content of
+			// data type of subject = is reference data type
+			// which type of reference - class type of ref , array type of ref, interface
+			// type of ref - (class type ref )
+			// are you cheking -- ref equality of string or content equality of string
+			// data type of subject is string and string class overriden equal
+
+		}
+		return false;
+
+	}
+}
---
+++ practice/day19video/src/tester/TestStudentEquals.java
@@ -0,0 +1,20 @@
+package tester;
+
+import com.app.students.Student;
+
+public class TestStudentEquals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student s1=new Student(101	,"react", "Prasad", "kale" , 90);
+		Student s2=new Student(101	,"angular", "Prasad", "kale" , 90);
+		System.out.println(s1.equals(s2));
+		
+		
+		
+		
+		
+		
+	}
+
+}
