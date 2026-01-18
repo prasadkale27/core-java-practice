11faf6534f2c15f982eded25e13a7452e806275a
prasadkale27 <prasadkale2727@gmail.com>
2026-01-18 20:59:48 +0530

    vehicle

--- practice/day17/src/com/app/vehicles/Tester.java
+++ practice/day17/src/com/app/vehicles/Tester.java
@@ -6,15 +6,18 @@ public class Tester {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter 1st vehicle details  : regNo,  color,  price");
-		Object vehicle1 = new Vehicle(sc.nextInt(), sc.next(), sc.nextDouble());
+		Vehicle vehicle1 = new Vehicle(sc.nextInt(), sc.next(), sc.nextDouble());
 		System.out.println("Enter 2nd  vehicle details  : regNo,  color,  price");
-		Object vehicle2 = new Vehicle(sc.nextInt(), sc.next(), sc.nextDouble());
+		Vehicle vehicle2 = new Vehicle(sc.nextInt(), sc.next(), sc.nextDouble());
 		// test equality of two vehicles
 		System.out.println(vehicle1.equals(vehicle2));// using inherited form of equals :based upon ref equality
+		System.out.println(vehicle1.hashCode()+""+vehicle2.hashCode());
+		System.out.println(vehicle1==vehicle2);//false check diffrent object
 		System.out.println(vehicle1.equals(sc));
-		Vehicle vehicle3=vehicle1;//copy of references  and it going to check the reference equality 
+		Vehicle vehicle3=vehicle1;//copy of references  and it going to check the reference equality
 		System.out.println("copy of ref ");
-		System.out.println(vehicle1.equals(vehicle3));//
+		System.out.println(vehicle1.equals(vehicle3));//same object pointing and its copy of ref true
+		
 		
 		
 		sc.close();
