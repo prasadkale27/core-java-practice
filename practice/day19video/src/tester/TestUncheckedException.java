646d84f19cdd62dc01c931b6797978b0916820cc
prasadkale27 <prasadkale2727@gmail.com>
2026-01-19 19:44:13 +0530

    Exception in thread "main" java.lang.NumberFormatException: For input string: "123abc" 	at java.base/jdk.internal.math.FloatingDecimal.readJavaFormatString(FloatingDecimal.java:2054) 	at java.base/jdk.internal.math.FloatingDecimal.parseDouble(FloatingDecimal.java:110) 	at java.base/java.lang.Double.parseDouble(Double.java:792) 	at day19video/tester.TestUncheckedException.main(TestUncheckedException.java:7)

--- practice/day19video/src/tester/TestUncheckedException.java
+++ practice/day19video/src/tester/TestUncheckedException.java
@@ -3,7 +3,7 @@ package tester;
 public class TestUncheckedException {
 	
 	public static void main(String[] args) {
-		String s="123.45";
+		String s="123abc";
 		System.out.println(Double.parseDouble(s));
 		System.out.println("code continues");
 	}
