f38f2b0f40076fa5c35801c91a99254608c6ffad
prasadkale27 <prasadkale2727@gmail.com>
2026-01-19 19:31:03 +0530

    TestUncheckedException

---
+++ practice/day19video/src/tester/TestUncheckedException.java
@@ -0,0 +1,12 @@
+package tester;
+
+public class TestUncheckedException {
+	
+	public static void main(String[] args) {
+		String s="123.45";
+		System.out.println(Double.parseDouble(s));
+		System.out.println("code continues");
+	}
+	
+
+}
