6036b2f2f6b3518fb882318ee6e59e1aa4466362
prasadkale27 <prasadkale2727@gmail.com>
2026-01-20 13:35:35 +0530

    AE

---
+++ practice/day19video/src/tester/Tester1.java
@@ -0,0 +1,24 @@
+package tester;
+
+public class Tester1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+		System.out.println("in main");
+		int c=100/0;//
+		System.out.println("result"+c);
+		String s="abcd";
+		System.out.println("parsed int value"+Integer.parseInt(s));//Number format exception 
+		s=null;
+		System.out.println("char at 0th index "+s.charAt(0));
+		int [] data= {1,2,3,4,5};
+		System.out.println("array data "+data[5]);
+		System.out.println("end of try...");
+		}catch(ArithmeticException e) {
+			System.out.println("1");
+		}
+		System.out.println("main over ....");
+	}
+
+}
