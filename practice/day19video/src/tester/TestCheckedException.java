bafc4b45230f12994d4ca9faa34783e3dcf5743f
prasadkale27 <prasadkale2727@gmail.com>
2026-01-20 13:32:59 +0530

    //jvm is the caller of main method and jvm is handled it 		//throws keyword ans exception handling deligation

--- practice/day19video/src/tester/TestCheckedException.java
+++ practice/day19video/src/tester/TestCheckedException.java
@@ -2,8 +2,9 @@ package tester;
 
 public class TestCheckedException {
 
-	public static void main(String[] args) {
-
+	public static void main(String[] args) throws InterruptedException {
+		//jvm is the caller of main method and jvm is handled it 
+		//throws keyword ans exception handling deligation 
 		System.out.println("before");
 		try {
 			Thread.sleep(5000);// InterruptedException -- checked exc -- javac forces handling of the checked
