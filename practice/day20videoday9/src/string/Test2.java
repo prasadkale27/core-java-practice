a61b6e6f18258a61716d58e96ce3996a4ecb69e6
prasadkale27 <prasadkale2727@gmail.com>
2026-01-26 19:12:09 +0530

    equal vs ref

--- practice/day20videoday9/src/string/Test2.java
+++ practice/day20videoday9/src/string/Test2.java
@@ -10,5 +10,15 @@ public class Test2 {
 		System.out.println(s1.equals(s2));// equals : inherited from java.lang.object -- has String class oerridden it
 			//yes - FOR (MODIFICATION - METHOD OERRIDING IS ACHIVE RUNTIME POLYMORPHISM )
 			//For replacing ref equality by content  equality and content of the String is (char sequence) : true 
+	
+		String s3=new String("TESTING12345");
+		System.out.println(s1==s3);//false - ref cheking 
+		System.out.println(s1.equals(s3));//false - it is case sensitive content equality case sensitive 
+		System.out.println(s1.equalsIgnoreCase(s3));//true - caontent equality case 
+		
+	
+	
 	}
 }
+//req equality means - address equality 
+//equals method == for content equality
