d43c6848af295e5f938e58cd83e25075890efe2c
prasadkale27 <prasadkale2727@gmail.com>
2026-01-27 16:18:47 +0530

    String s3=new  String("java Strings are easier than  in c++"); 				System.out.println(s3.indexOf("Strings")+" "+s3.lastIndexOf("Strings"));// 5   5

--- practice/day20videoday9/src/string/Test4.java
+++ practice/day20videoday9/src/string/Test4.java
@@ -21,8 +21,8 @@ public class Test4 {
 				//print yes if s2 	contains "API" else print no
 				System.out.println(s2.contains("API")?"YES":"NO");
 				
-				String s3=new  String("java Strings are easier than Strings in c++");
-				System.out.println(s3.indexOf("Strings")+" "+s3.lastIndexOf("Strings1234"));//no such occurence so it print -1
+				String s3=new  String("java Strings are easier than  in c++");
+				System.out.println(s3.indexOf("Strings")+" "+s3.lastIndexOf("Strings"));// 5   5
 				
 				
 				
