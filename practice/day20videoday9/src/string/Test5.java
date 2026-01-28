aa51a26e847735c79a2bad4736420a79d0337533
prasadkale27 <prasadkale2727@gmail.com>
2026-01-28 12:58:05 +0530

    append() method चा वापर StringBuilder किंवा StringBuffer मध्ये नवीन text/value शेवटी जोडण्यासाठी केला जातो.

---
+++ practice/day20videoday9/src/string/Test5.java
@@ -0,0 +1,22 @@
+package string;
+
+public class Test5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		StringBuilder sb1=new StringBuilder("hello");
+		System.out.println("length"+sb1.length()+  "  capa " + sb1.capacity());//5  capacity =16+5=21
+		sb1.append(true);
+		sb1.append(123.45);
+		sb1.append(34567);
+		
+		System.out.println(sb1);//sb1.toString
+		//hellotrue123.4534567
+		
+		
+		
+		
+	}
+
+}
