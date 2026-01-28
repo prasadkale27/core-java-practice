77a3be6f5fc3b104032d6517957cf63042f5369d
prasadkale27 <prasadkale2727@gmail.com>
2026-01-28 14:42:10 +0530

    insert method of stringbuilderr insert() method चा उपयोग StringBuilder मध्ये मध्यभागी किंवा सुरुवातीला कोणताही data insert करण्यासाठी होतो.

--- practice/day20videoday9/src/string/Test5.java
+++ practice/day20videoday9/src/string/Test5.java
@@ -5,16 +5,18 @@ public class Test5 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		StringBuilder sb1=new StringBuilder("hello");
-		System.out.println("length"+sb1.length()+  "  capa " + sb1.capacity());//5  capacity =16+5=21
+		StringBuilder sb1 = new StringBuilder("hello");
+		System.out.println("length" + sb1.length() + "  capa " + sb1.capacity());// 5 capacity =16+5=21
 		sb1.append(true);
 		sb1.append(123.45);
-		sb1.append(34567);
+		StringBuilder sb2 = sb1.append(34567);
 		
-		System.out.println(sb1);//sb1.toString
-		//hellotrue123.4534567
-		
-		
+		System.out.println(sb1);// sb1.toString , concateneted contents
+		// hellotrue123.4534567
+		System.out.println(sb2);
+		System.out.println(sb1==sb2);//true
+		sb1.insert(2, 999999);
+		System.out.println(sb2);
 		
 		
 	}
